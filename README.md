e917b5919ecc2f156c42a64093c8bc481a462ee3
김보미 <kimbomi2172@naver.com>
2023-03-12 16:52:15 +0900

    2주차

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Practice_vuejs
 
-`https://vuejs.org/guide/introduction.html`
+https://vuejs.org/guide/introduction.html <br>
+
+css 프레임워크 <br>
+https://vuetifyjs.com/en/ 
---
+++ helloworld/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-text-field v-model="message"></v-text-field>
+
+      {{ message }}
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    message:"안녕하세요. 저는 김보미입니다."
+  }),
+}
+</script>
