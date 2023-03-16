4006536a5956e4124ed5685cf195f129a2081a7f
unknown <kimbomi2172@naver.com>
2023-03-16 11:10:08 +0900

    3주차 수업

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
 # Practice_vuejs
+📘소프트웨어 공학 수업 정리 노트📘
 
 https://vuejs.org/guide/introduction.html <br>
 
 css 프레임워크 <br>
 https://vuetifyjs.com/en/ 
+
+<hr>
+
+### DOM
+https://miro.medium.com/v2/resize:fit:640/1*NA2VKR09ECb8PEgYDteR3w.gif <br>
+-> VUE는 가상의 DOM을 만들어 사용자의 행동에 따라 생겨야 할 태그를 만들어(가공) 내보냄.
+
+### 이벤트
+- v-btn에 @click 등 @를 통해 이벤트를 추가할 수 있음.
+```vue
+<v-btn @click="helloWorld()">안녕하세요</v-btn>
+```
+
+### 배열에 있는 데이터 출력하기
+- 변수 선언
+```js
+data() {
+    return {
+      students: ["김보미", "김세민", "김혜현", "나호윤"]
+    }
+  }
+```
+- 반복문 for로 출력하기
+
+```vue
+<div v-for="student in students">
+    {{ student }}
+</div>
+```
---
+++ workspace/helloworld/src/views/HomeView.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <v-btn @click="helloWorld()">안녕하세요</v-btn>
+
+    <!-- <div v-if="onoff"></div> -->
+
+    <!-- 반복문 for -->
+    <!-- old클래스는 학생의 나이가 40이상만 출력함. -->
+    <div v-for="student in studentFilter()" class="student" :class="{old:student.age>=40}">
+      {{ student.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomeView',
+  // 변수 선언
+  data() {
+    return {
+      onoff: true,
+      students: [
+        {name : "김보미", age:20},
+        {name : "장세림", age:30},
+        {name : "윤다영", age:40},
+        {name : "임효진", age:50}
+      ]
+    }
+  },
+
+  methods:{
+    helloWorld() {
+      // this.students.push("학생" + Math.random())
+      // this.onoff = !this.onoff
+      this.$router.push("/helloworld")
+    },
+    studentFilter() {
+      var returnArr=[]
+      if(this.onoff == true) {
+        this.students.forEach(student => {
+          returnArr.push(student)
+        })
+      } else {
+        // 나이가 40미만인 학생만 리턴
+        this.students.forEach(student => {
+          if(student.age < 40) {
+            returnArr.push(student)
+          }
+        })
+      } 
+      return returnArr;
+    }
+  }
+});
+</script>
+
+<style>
+  .student {
+    font-size: 20px;
+    margin-bottom: 10px;
+  }
+
+  .student.old {
+    color: red;
+  }
+</style>
